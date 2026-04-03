45fee6f4ee5cc16bd2fd1952a8ce5ef9b5af51fd
yujie zhao <yujiezhao@yujiedeMacBook-Air-2023.local>
2026-04-03 05:28:51 -0500

    feat: 修改时间

--- docs/.vitepress/cache/deps/@theme_index.js
+++ docs/.vitepress/cache/deps/@theme_index.js
@@ -9,38 +9,38 @@ import {
 } from "./chunk-SNNOYR6U.js";
 
 // node_modules/vitepress/dist/client/theme-default/index.js
-import "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
+import "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
 
 // node_modules/vitepress/dist/client/theme-default/without-fonts.js
-import "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
-import "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/styles/base.css";
-import "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/styles/icons.css";
-import "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
-import "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
-import "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
-import "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
-import "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
-import "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
-import VPBadge from "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import Layout from "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/Layout.vue";
-import { default as default2 } from "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import { default as default3 } from "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
-import { default as default4 } from "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
-import { default as default5 } from "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPFeatures.vue";
-import { default as default6 } from "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPHomeContent.vue";
-import { default as default7 } from "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
-import { default as default8 } from "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
-import { default as default9 } from "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
-import { default as default10 } from "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
-import { default as default11 } from "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPLink.vue";
-import { default as default12 } from "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPNavBarSearch.vue";
-import { default as default13 } from "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPSocialLink.vue";
-import { default as default14 } from "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPSocialLinks.vue";
-import { default as default15 } from "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
-import { default as default16 } from "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
-import { default as default17 } from "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
-import { default as default18 } from "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
-import { default as default19 } from "/Users/yujiezhao/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
+import "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
+import "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/styles/base.css";
+import "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/styles/icons.css";
+import "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
+import "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
+import "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
+import "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
+import "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
+import "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
+import VPBadge from "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import Layout from "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/Layout.vue";
+import { default as default2 } from "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import { default as default3 } from "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
+import { default as default4 } from "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
+import { default as default5 } from "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPFeatures.vue";
+import { default as default6 } from "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPHomeContent.vue";
+import { default as default7 } from "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
+import { default as default8 } from "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
+import { default as default9 } from "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
+import { default as default10 } from "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
+import { default as default11 } from "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPLink.vue";
+import { default as default12 } from "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPNavBarSearch.vue";
+import { default as default13 } from "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPSocialLink.vue";
+import { default as default14 } from "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPSocialLinks.vue";
+import { default as default15 } from "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
+import { default as default16 } from "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
+import { default as default17 } from "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
+import { default as default18 } from "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
+import { default as default19 } from "/Users/yujiezhao/Desktop/nn-vp-doc/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
 
 // node_modules/vitepress/dist/client/theme-default/composables/local-nav.js
 import { onContentUpdated } from "vitepress";
--- docs/career/bagu/js.md
+++ docs/career/bagu/js.md
@@ -1,5 +1,50 @@
 # 前端八股
 JS，框架，网络，浏览器部分
 ## 网络
-### 协议版本
-1. HTTP 1.0 和 HTTP 1.1 之间有哪些区别？
+### 协议
+#### 1. http结构    
+1. 请求：      
+- 请求⾏    
+方法，url，协议版本
+- 请求头部    
+头名：带的值  若干个
+- 空⾏
+- 请求体     
+带着的数据，payload等等。     
+2. 响应：    
+- 响应⾏     
+协议版本  状态码。  状态吗原因短语。  
+- 响应头     
+同样类似请求头的元信息。    
+- 空⾏
+- 响应体     
+服务器相应的数据
+#### 2. http请求方法    
+1. 常见方法      
+- GET：用于获取资源，是幂等的，参数一般通过URL query传参数；
+- POST：用于提交数据或创建资源，不幂等，数据在请求体中；
+- PUT：用于替换资源，是幂等的
+- PATCH：用于部分更新资源；
+- DELETE：用于删除资源，是幂等的；
+- HEAD：只返回响应头；
+- OPTIONS：用于查询服务器支持的方法，在 CORS 中用于预检请求；   
+    
+   
+2. get post区别        
+- GET 语义上用于读取资源，通常是安全且幂等的；POST 用于提交数据或触发操作，通常不是幂等的
+- 默认场景下一般对get缓存    
+- GET 请求通常通过 URL query 参数传参，一般不使用请求体；POST 请求通常通过请求体传递数据     
+- GET 请求参数会写在url里，post不会，一般走body，因为同样原因，get查询参数有限      
+3. put和post
+- 核心区别是put幂等，post不是，可以理解为put纯更新，post发上去后端想干啥干啥
+4. option预检。    
+复杂请求，非get，post。     
+application/x-www-form-urlencoded
+multipart/form-data
+text/plain      
+提前发预检请求，如果不符阻断。    
+看    
+Access-Control-Allow-Origin
+Access-Control-Allow-Methods
+Access-Control-Allow-Headers
+#### 3. 
